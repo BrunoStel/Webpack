329decc502625f6b1d2f235a656694e57b61ca84
Bruno Stelmastchuk <brunostel@gmail.com>
2021-10-24 08:06:36 -0300

    Adicionando components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import $ from 'jquery';
 import './CSS/style.css';
 import imagem from './images/c1.jpg'
+import Botao from './components/botao/index.js'
 
 $('#imagem').attr('src', imagem)
 
 $(function(){
-    alert('Estou funcionando')
+    let botao = new Botao()
+    botao.setTitle('Testador')
+    botao.setClick(()=>{alert('Clicou!')})
+
+    $('.area').html(botao.render())
+
 })
